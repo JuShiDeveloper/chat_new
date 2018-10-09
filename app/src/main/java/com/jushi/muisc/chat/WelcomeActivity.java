9717638776a7e411a210ed351f351d643b21534d
WYF <wyfjushi@gmail.com>
2018-10-09 11:07:27 +0800

    将应用加入到音频打开方式选项中

--- app/src/main/java/com/jushi/muisc/chat/WelcomeActivity.java
+++ app/src/main/java/com/jushi/muisc/chat/WelcomeActivity.java
@@ -3,11 +3,14 @@ package com.jushi.muisc.chat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.jushi.muisc.chat.main.MainActivity;
 import com.jushi.muisc.chat.music.common.utils.music.LocalMusicUtils;
@@ -23,12 +26,21 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(); //不设置View,在manifest中设置Theme,让欢迎页面能快速启动
+        Intent intent = getIntent();
+        if (intent != null) {
+            String action = intent.getAction();
+            if (!TextUtils.isEmpty(action) && Intent.ACTION_VIEW.equals(action)) {
+                Uri uri = intent.getData();
+                String audioPath = Uri.decode(uri.getEncodedPath());
+                Log.v("==yufei==", "audioPath = " + audioPath);
+            }
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        final Intent intent = new Intent(this,MainActivity.class);
+        final Intent intent = new Intent(this, MainActivity.class);
         LocalMusicUtils.getSongs(this);
         requestPermission();
         PATH.initPath();
@@ -38,7 +50,7 @@ public class WelcomeActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },1500);
+        }, 1500);
         MobclickAgent.onResume(this);
     }
 
